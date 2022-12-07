bff6535fe1647d8e3ebbd6e796008c7f522b37ae
DANNJUGUNA <ndan30071@gmail.com>
2022-12-07 10:28:03 +0300

    nice commit

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Add your code here
+function submitData(username,useremail){
+ fetch(" http://localhost:3000/users",{
+    method:"POST",
+    headers:{
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body:JSON.stringify({
+      email:useremail,
+      name:username
+    })
+  })
+  .then(response=>response.json())
+  .then()
+  .catch((error)=>{
+    alert("error message")
+    console.log(error.message);
+  });
+
+}
+submitData("dan","dan567@gmail.com")
